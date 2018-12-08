0692e5e5c7cd03979277dc3e9ce11915ca597295
Rémi Caumette <remicaumette@icloud.com>
2018-12-08 01:18:11 +0100

    hello world

--- cmd/expected-test/main.go
+++ cmd/expected-test/main.go
@@ -1,34 +1,71 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"io/ioutil"
+	"github.com/containerd/go-cni"
+	"github.com/containerd/go-runc"
+	"io"
+	"log"
 	"os"
-	"os/exec"
 )
 
 func checkErr(err error) {
 	if err != nil {
-		fmt.Printf("err: %v\n", err)
+		log.Output(2, fmt.Sprintf("an error occurred: %v\n", err))
 		os.Exit(1)
 	}
 }
 
+func setupNetworking(runtime runc.Runc) {
+	state, err := runtime.State(context.Background(), "hello")
+	checkErr(err)
+	log.Printf("%v\n", state.Pid)
+	networking, err := cni.New(cni.WithMinNetworkCount(2),
+		cni.WithPluginConfDir("/home/vagrant/go/src/gitlab.com/expected.sh/agent/net"),
+		cni.WithPluginDir([]string{"/opt/cni/"}),
+		 )
+
+	checkErr(err)
+	checkErr(networking.Load(cni.WithLoNetwork, cni.WithDefaultConf))
+	result, err := networking.Setup("eth0", fmt.Sprintf("/proc/%v/ns/net", state.Pid), cni.WithCapabilityPortMap([]cni.PortMapping{
+		{
+			HostPort: 8080,
+			ContainerPort: 80,
+			Protocol: "tcp",
+		},
+	}))
+	checkErr(err)
+	log.Printf("%v\n", result)
+}
+
 func main() {
-	r, w, err := os.Pipe()
+	runtime := runc.Runc{}
+	socket, err := runc.NewTempConsoleSocket()
 	checkErr(err)
 
-	cmd := exec.Command("runc", "create", "test")
-	//cmd := exec.Command("sh", "-c", "echo hello world")
-	cmd.Stdout = w
-
-	in, _ := cmd.StdinPipe()
-	in.Close()
-	checkErr(cmd.Run())
-	w.Close()
-
-	fmt.Printf("%v\n", cmd.ProcessState.Success())
-	out, err := ioutil.ReadAll(r)
+	containers, err := runtime.List(context.Background())
 	checkErr(err)
-	fmt.Printf("%v", string(out))
+	for _, container := range containers {
+		if container.ID == "hello" {
+			checkErr(runtime.Delete(context.Background(), "hello", &runc.DeleteOpts{
+				Force: true,
+			}))
+		}
+	}
+
+	defer socket.Close()
+	err = runtime.Create(context.Background(), "hello", "/home/vagrant/go/src/gitlab.com/expected.sh/agent", &runc.CreateOpts{
+		ConsoleSocket: socket,
+	})
+	checkErr(err)
+	err = runtime.Start(context.Background(), "hello")
+	checkErr(err)
+	setupNetworking(runtime)
+	console, err := socket.ReceiveMaster()
+	checkErr(err)
+	go func() {
+		io.Copy(os.Stdout, console)
+	}()
+	io.Copy(console, os.Stdin)
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,11 @@
 module gitlab.com/expected.sh/agent
+
+require (
+	github.com/containerd/console v0.0.0-20181022165439-0650fd9eeb50 // indirect
+	github.com/containerd/go-cni v0.0.0-20181203145209-c399b1539b8e // indirect
+	github.com/containerd/go-runc v0.0.0-20180907222934-5a6d9f37cfa3 // indirect
+	github.com/containernetworking/cni v0.6.0 // indirect
+	github.com/opencontainers/runtime-spec v1.0.1 // indirect
+	github.com/pkg/errors v0.8.0 // indirect
+	golang.org/x/sys v0.0.0-20181206074257-70b957f3b65e // indirect
+)
